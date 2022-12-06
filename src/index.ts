963cfdf39173670440563856169b00ac1d92720b
Matija Novosel <mnovosel5@gmail.com>
2022-12-06 22:46:08 +0100

    Correct types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export const intersect = (a: number[], b: number[]) => {
+export const intersect = (a: any[], b: any[]) => {
   const setB = new Set(b);
   return [...new Set(a)].filter((x) => setB.has(x));
 };
 
-export const chunk = (arr: number[], chunkSize: number) => {
+export const chunk = (arr: any[], chunkSize: number) => {
   const result = [];
   for (let i = 0, len = arr.length; i < len; i += chunkSize)
     result.push(arr.slice(i, i + chunkSize));
@@ -16,7 +16,7 @@ export const range = (start: number, end: number) => {
     .map((_, idx) => start + idx);
 };
 
-export const splitByValue = (arr: number[], val: number) => {
+export const splitByValue = (arr: any[], val: any) => {
   const result = [[]];
   for (let i = 0; i < arr.length; i++)
     if (arr[i] == val) result.push([]);
