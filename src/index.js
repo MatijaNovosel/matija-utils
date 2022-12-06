735bbf2d0df4c84c78418ea3e060530312ade6da
Matija Novosel <mnovosel5@gmail.com>
2022-12-06 21:35:49 +0100

    Testing workflow

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ export const intersect = (a, b) => {
   const setB = new Set(b);
   return [...new Set(a)].filter((x) => setB.has(x));
 };
+
+export const chunk = (arr, chunkSize) => {
+  const result = [];
+  for (let i = 0, len = arr.length; i < len; i += chunkSize)
+    result.push(arr.slice(i, i + chunkSize));
+  return result;
+};
